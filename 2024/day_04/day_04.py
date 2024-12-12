1266a692a424e9061445dc6fa774e3640a0ec84d
Niall Hurley <niall.hurley@gmail.com>
2024-12-12 17:47:05 +0000

    day 4 (moved over from colab)

--- 2024/day_04/day_04.py
+++ 2024/day_04/day_04.py
@@ -1,5 +1,5 @@
 #%%
-daynum = ""
+daynum = "04"
 import numpy as np
 
 with open(f"day_{daynum}_test_input.txt", "r") as f:
@@ -11,16 +11,76 @@ with open(f"day_{daynum}_input.txt", "r") as f:
 
 #%%
 
+# generate all directions (lazily!)
+
+import itertools
+dirs = []
+for r in itertools.product([-1,0,1], [-1,0,1]):
+    dirs.append((r[0], r[1]))
+dirs   
+
 # %%
 def part1(txt):
-    return     
+    # Part 1
+    tmp = []
+    for l in txt:
+        tmp.append([(x) for x in l])
+
+    tmp = np.array(tmp)
+    R,C = tmp.shape
+
+    cnt=0
+
+    for r in range(R):
+        for c in range(C):
+            # print((r,c))
+            for dir in dirs:
+                dr, dc = dir
+                if (0<=r+dr*3<=R-1 and  0<=c+dc*3<=C-1):
+                    if 'XMAS' == tmp[r,c]+tmp[r+dr*1,c+dc*1]+tmp[r+dr*2,c+dc*2]+tmp[r+dr*3,c+dc*3]:
+                        cnt+=1
+
+    return cnt
+    
+
+  
 
 print(f"Part 1 (test): {part1(test_txt)}")
 print(f"Part 1: {part1(input_txt)}")
 
 # %%
 def part2(txt):
-    return 0
+    # Part2
+    tmp = []
+    for l in txt:
+        tmp.append([(x) for x in l])
+
+    tmp = np.array(tmp)
+    R,C = tmp.shape
+    cnt=0
+
+    for r in range(R):
+        for c in range(C):
+            # print((r,c))
+            local_cnt = 0
+            local_tmp =[]
+            for dir in [(-1, -1), (-1, 1),  (1, -1), (1, 1)]: # only diagonals!
+                dr, dc = dir
+
+                if tmp[r,c]=='A':
+
+                    if (r-dr)<0 or (r+dr)<0     or (r-dr)>R-1 or (r+dr)>R-1    or (c+dc)<0 or (c-dc)<0   or (c+dc)>C-1  or (c-dc)>C-1:
+                        # we've hit a boundary
+                        pass
+                    else:
+                        str1 = tmp[r-dr,c-dc]+tmp[r+0,c+0]+tmp[r+dr,c+dc]
+                        if 'MAS' == str1:
+                            local_cnt+=1
+                            # local_tmp.append([(r,c),dir, str1])
+            if local_cnt>1:
+                cnt+=1
+        
+    return cnt
 
 print(f"Part 2 (test): {part2(test_txt)}")
 print(f"Part 2: {part2(input_txt)}")
